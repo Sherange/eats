9fa431e8746aac20e094d30bd38d74cc4226145b
sherange <sherangef@gmail.com>
2018-11-17 15:04:15 +0530

    changes

--- src/components/user-profile/Profile.js
+++ src/components/user-profile/Profile.js
@@ -1,39 +1,56 @@
 import React, { Component } from "react";
 import TextField from "material-ui/TextField";
 import DatePicker from "material-ui/DatePicker";
+import SelectField from "material-ui/SelectField";
+import MenuItem from "material-ui/MenuItem";
+import { GENDER } from "../../constant/constant.js";
 
 class Profile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      id : null,
-      name: "",
-      email: "",
-      imagePath: "",
-      phoneNumber: "",
-      dateOfBirth: "",
-      gender: ""
+      user: []
     };
   }
-  componentDidMount() {
-  }
+  componentDidMount() {}
 
   static getDerivedStateFromProps(nextProps, prevState) {
-    if (nextProps.user.id !== prevState.id) {
-      return {
-        id : nextProps.user.id,
-        name: nextProps.user.name,
-        email: nextProps.user.email,
-        imagePath: nextProps.user.image_path,
-        phoneNumber: nextProps.user.phone_number,
-        dateOfBirth: nextProps.user.description
-      };
+    if (nextProps.user.id !== prevState.user.id) {
+      return { user: nextProps.user };
     }
     return null;
   }
 
+  handleChangePhoneNumber = event => {
+    const { user } = this.state;
+    user.phone_number = event.target.value;
+    this.setState({ user });
+  };
+
+  handleChangeGender = (event, index, value) => {
+    const { user } = this.state;
+    const selectedGender = GENDER.find((item, key) => {
+      return item.key === index;
+    });
+    user.gender = selectedGender.value;
+    this.setState({ user });
+  };
+
+  handleChangeDescription = event => {
+    const { user } = this.state;
+    user.description = event.target.value;
+    this.setState({ user });
+  };
+
   render() {
-    console.log("this.props.user", this.props.user);
+    const genderItem = GENDER.map((item, key) => {
+      return <MenuItem key={key} value={key} primaryText={item.value} />;
+    });
+
+    const selectedGender = GENDER.findIndex((item, index) => {
+      return item.value == this.state.user.gender;
+    });
+
     return (
       <div className="container-fluid">
         <div className="user-profile-wrapper">
@@ -45,23 +62,45 @@ class Profile extends Component {
             />
             <p className="profile-label">User Details</p>
             <hr className="profile-hr" />
-
-            <p className="profile-email">{ this.state.email}</p>
+            <p className="profile-email">{this.state.user.email}</p>
             <br />
 
             <p className="profile-label">Address</p>
             <hr className="profile-hr" />
+            <p className="profile-address">
+              {this.state.user.user_address &&
+                this.state.user.user_address.address}
+            </p>
+            <p className="profile-address">
+              {this.state.user.user_address &&
+                this.state.user.user_address.street_one}
+            </p>
+            <p className="profile-address">
+              {this.state.user.user_address &&
+                this.state.user.user_address.street_two}
+            </p>
+            <br />
 
-            <p className="profile-address">No : 68/4 Parakrama Mawatha</p>
-            <p className="profile-address">Panadura</p>
-            <p className="profile-address">Sri Lanka</p>
+            <p className="profile-label">City</p>
+            <hr className="profile-hr" />
+            <p className="profile-address">
+              {this.state.user.user_address &&
+                this.state.user.user_address.city}
+            </p>
+            <br />
+
+            <p className="profile-label">Country</p>
+            <hr className="profile-hr" />
+            <p className="profile-address">
+              {this.state.user.user_address &&
+                this.state.user.user_address.country}
+            </p>
             <br />
 
             <p className="profile-label">Description</p>
             <hr className="profile-hr" />
             <p className="profile-address">
-              dsfsdf sfas fsdf sfsd sdfsdf sdfsdf sd sdfsdfsdf sdsadadaS DASDASD
-              SDSADASD ASDASDAS SADASA DSas dasdasd sdasdasd asdsad dsadAS{" "}
+              {this.state.user && this.state.user.description}
             </p>
           </div>
 
@@ -72,7 +111,7 @@ class Profile extends Component {
               className="user-profile-image-sm"
               alt="userImage"
             />
-            <p className="profile-name">{this.state.name}</p>
+            <p className="profile-name">{this.state.user.name}</p>
 
             <div className="box-one">
               <p className="profile-rating">User Rating</p>
@@ -94,22 +133,23 @@ class Profile extends Component {
                 <h4 className="profile-from-label">Email :</h4>
               </div>
               <div className="col-md-10">
-                <h4 className="profile-from-text">{this.state.email}</h4>
+                <h4 className="profile-from-text">{this.state.user.email}</h4>
               </div>
             </div>
 
             <div className="row">
               <div className="col-md-2">
-                <h4 className="profile-from-label">Address :</h4>
+                <h4 className="profile-from-label">Phone Number :</h4>
               </div>
               <div className="col-md-10">
-                <p
-                  className="profile-from-address"
-                  style={{ marginTop: "10px" }}
-                >
-                  No : 68/4 Parakrama MAwatha
-                </p>
-                <p className="profile-from-address">Panadura</p>
+                {/* <p className="profile-from-address" /> */}
+                <TextField
+                  type="text"
+                  id="user-phone-number"
+                  value={String(this.state.user.phone_number)}
+                  onChange={this.handleChangePhoneNumber}
+                  style={{ width: "90%" }}
+                />
               </div>
             </div>
 
@@ -118,11 +158,10 @@ class Profile extends Component {
                 <h4 className="profile-from-label">Birth Date :</h4>
               </div>
               <div className="col-md-10">
-                <p className="profile-from-text">1st Aug 1996</p>
+                {/* <p className="profile-from-text">1st Aug 1996</p> */}
                 <DatePicker
                   hintText="Controlled Date Input"
-                  value={this.state.controlledDate}
-                  onChange={this.handleChange}
+                  style={{ width: "90%" }}
                 />
               </div>
             </div>
@@ -132,7 +171,65 @@ class Profile extends Component {
                 <h4 className="profile-from-label">Gender :</h4>
               </div>
               <div className="col-md-10">
-                <TextField hintText="Hint Text" />
+                <SelectField
+                  value={selectedGender}
+                  onChange={this.handleChangeGender}
+                  style={{ width: "90%" }}
+                  // errorText={this.state.errorOpeningHours}
+                >
+                  {genderItem}
+                </SelectField>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-2">
+                <h4 className="profile-from-label">Description :</h4>
+              </div>
+              <div className="col-md-10">
+                <TextField
+                  id="user-description"
+                  value={this.state.user.description}
+                  onChange={this.handleChangeDescription}
+                  style={{ width: "90%" }}
+                  multiLine={true}
+                  rows={1}
+                  rowsMax={5}
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-2">
+                <h4 className="profile-from-label">Address :</h4>
+              </div>
+              <div className="col-md-10">
+                {/* <p className="profile-from-address" /> */}
+                <TextField hintText="address" />
+                <br />
+                <TextField hintText="street" />
+                <br />
+                <TextField hintText="street" />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-2">
+                <h4 className="profile-from-label">City :</h4>
+              </div>
+              <div className="col-md-10">
+                {/* <p className="profile-from-address" /> */}
+                <TextField hintText="" />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-2">
+                <h4 className="profile-from-label">Country :</h4>
+              </div>
+              <div className="col-md-10">
+                {/* <p className="profile-from-address" /> */}
+                <TextField hintText="" />
               </div>
             </div>
           </div>
---
+++ src/constant/constant.js
@@ -0,0 +1,1 @@
+export const GENDER = [{ key: 0, value: "Male" }, { key: 1, value: "Female" }];
